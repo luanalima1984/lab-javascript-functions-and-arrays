b8714e4d0015623a4db33b76570b2a041687ca6d
luanalima1984 <luana.lima@tjdft.jus.br>
2022-10-20 21:34:45 -0300

    interação 1

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,19 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+
+  if(num1>num2){
+    return num1
+  }
+
+  if (num1<num2){
+    return num2;
+  }
+
+  if (num1 === num2) {
+    return num1;
+  }
+
+}
 
 
 
